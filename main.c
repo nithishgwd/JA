80436cc06644af12aa1e29ad59a31a980730c718
epic-code68 <113482683+epic-code68@users.noreply.github.com>
2023-01-17 08:54:08 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,temp,min,n,a[100];
+    printf("Enter the size\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        min=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+
+                min=j;
+            }
+
+        }
+        if(min!=i)
+        {
+            temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
